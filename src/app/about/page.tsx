c204c9156c80480363bb48fc5239a165f2ee180f
legolasblack <legolas.black.ds@gmail.com>
2024-09-21 23:55:43 -0600

    dejamos fija la aqrquitectura ejemplo para proyectos nuevos

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <p>
+            Acerca de este archivo
+        </p>
+        <p>
+            este archivo representara la sintaxis que vayamos recopilando dentro de la sintaxis de Next.js
+        </p>
+    </div>
+  )
+}
