932a7549d7c131bdf49566837ea27586eeacae81
legolasblack <legolas.black.ds@gmail.com>
2024-09-29 19:40:20 -0600

    ya tenemos configurado tailwind y no lo hacemos jalar tiene un error

--- src/app/NextUi/page.tsx
+++ src/app/NextUi/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+/*Page main NextUi */
+
+import React from 'react';
+
+export default function Page() {
+  return (
+   <>
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500 flex items-center justify-center">
+      <p className="text-white text-2xl font-semibold">Implementación de NextUI</p>
+    </div>
+   
+   </>
+  );
+}
+
+
+
+/* import React from 'react'
 
 export default function page() {
   return (
     <div>
-        <p>implementacion de NextUi</p>
+        <p className=''>implementacion de NextUi</p>
     </div>
   )
-}
+} */
