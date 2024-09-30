3c22d862ebde358f6a234038820b1b4ed15a0465
legolasblack <legolas.black.ds@gmail.com>
2024-09-30 17:51:14 -0600

    dejamos listo el primer modal para calar la libreria Nextui el modal ya contiene algunos inputs solo falta ponerle funcionabilidad el form

--- src/app/NextUi/page.tsx
+++ src/app/NextUi/page.tsx
@@ -1,18 +1,48 @@
 /*Page main NextUi */
+'use client'
 import {Button} from '@nextui-org/button';
+import {  Modal,   ModalContent,   ModalHeader,   ModalBody,   ModalFooter, useDisclosure, Input} from "@nextui-org/react";
 
 import React from 'react';
 
 export default function Page() {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
   return (
    <>
     <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500 flex items-center justify-center">
       <p className="text-white text-2xl font-semibold">Implementación de NextUI</p>
       <div>
-        <Button className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" isDisabled>Click me please!!</Button>
+        <Button className="bg-gradient-to-tr from-red-600 to-yellow-500 text-white shadow-lg" onPress={onOpen}>Open Modal</Button>
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="">Registro de nueva incidencia</ModalHeader>
+              <ModalBody>
+              <Input type="email" label="Email" />
+              <Input type="email" label="Email" placeholder="Enter your email" />
+              <Input type="email" label="Email" />
+              <Input type="email" label="Email" placeholder="Enter your email" />
+              <Input type="email" label="Email" />
+              <Input type="email" label="Email" placeholder="Enter your email" />
+              <Input type="email" label="Email" />
+              <Input type="email" label="Email" placeholder="Enter your email" />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       </div>
-    </div>
-   
+    </div>   
    </>
   );
 }
